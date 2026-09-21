32aa941c394c896f9a75311151081eb41fcc4752
nikgSkew <148353169+nikgSkew@users.noreply.github.com>
2026-09-21 15:07:21 +0400

    day 13

--- Day 12/Number Guessing Project/main.py
+++ Day 12/Number Guessing Project/main.py
@@ -4,25 +4,25 @@ import random
 
 print(art.logo)
 
-number_to_guess = random.randint(1,100)
-turns_map={"e":10,"h":5}
-turns=turns_map[input("you want easy or hard?(e/h)\n")]
-print(f"You have {turns} turns left\n")
+NUMBER_TO_GUESS = random.randint(1, 100)
+TURNS_MAP={"e":10, "h":5}
+TURNS=TURNS_MAP[input("you want easy or hard?(e/h)\n")]
+print(f"You have {TURNS} turns left\n")
 
 
 def is_the_number(guess):
-    if guess>number_to_guess:
+    if guess>NUMBER_TO_GUESS:
         print("Too high\n")
-    elif guess<number_to_guess:
+    elif guess<NUMBER_TO_GUESS:
         print("Too low\n")
-    elif guess==number_to_guess:
+    elif guess==NUMBER_TO_GUESS:
         print("You guessed the number, you win\n")
         return True
     return False
 
-while turns > 0 and not is_the_number(int(input("enter your guess?\n"))):
-      turns -= 1
-      print(f"You have {turns} turns left\n")
-
-if turns==0:
-    print(f"Number of chances finish, you loose, the number was {number_to_guess}\n")
+while not is_the_number(int(input("enter your guess?\n"))):
+      TURNS -= 1
+      print(f"You have {TURNS} turns left\n")
+      if TURNS==0:
+        print(f"Number of chances finish, you loose, the number was {NUMBER_TO_GUESS}\n")
+        break
--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,7 @@
-age = int(input("How old are you?"))
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+    print("Sorry, you entered wrong input, you shall insert your aga in numeric form, ex 12r")
+    age = int(input("How old are you?"))
 if age > 18:
-print("You can drive at age {age}.")
+    print(f"You can drive at age {age}.")
