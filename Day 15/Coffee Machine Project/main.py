284b95ae8e8d1853df8455d967acb7b8a266cc90
nikgSkew <148353169+nikgSkew@users.noreply.github.com>
2026-09-21 17:24:08 +0400

    day 15

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -33,7 +33,7 @@ resources = {
 balance = 0
 def print_report():
     print(f"\nwater - {resources['water']}\nmilk - {resources['milk']}\ncoffee - {resources['coffee']}\nbalance - "
-          f"{balance}\n")
+          f"{round(balance,2)}\n")
 
 def collect_money(money_to_collect):
     print(f"You need to pay {money_to_collect} dollars.")
@@ -45,60 +45,55 @@ def collect_money(money_to_collect):
     collected_money= (0.25*quater) + (0.1*dimes) + (0.05*nickel) + (0.01*pennies)
     global balance
     if collected_money>money_to_collect:
-        print(f"Thank you for paying {collected_money}, Refunding you extra {collected_money-money_to_collect} dollars")
-        balance += collected_money
+        print(f"Thank you for paying {round(collected_money,2)}, Refunding you extra "
+              f"{round(collected_money-money_to_collect,2)} dollars")
+        balance=round(balance + money_to_collect,2)
         return True
     if collected_money==money_to_collect:
         print(f"Thank you for paying {collected_money}")
-        balance += collected_money
+        balance=round(balance + money_to_collect,2)
         return True
     print(f"Not enough money, refunding you {collected_money} dollars")
     return False
 
-def enough_resources(drink,water, milk, coffee):
-    if resources["water"]<water or resources["milk"]<milk or resources["coffee"]<coffee:
-        print(f"Not enough resources for a {drink}, refunding your money.")
-        return False
+def enough_resources(drink_name, ingredients):
+    for item in ingredients:
+        if resources[item]<ingredients[item]:
+            print(f"Not enough resources for a {drink_name}.")
+            return False
     return True
 
-def consume_resources(water, milk, coffee):
-    resources["water"]=resources["water"]-water
-    resources["milk"]=resources["milk"]-milk
-    resources["coffee"]=resources["coffee"]-coffee
+def make_drink(ingredients):
+    for item in ingredients:
+        resources[item]-=ingredients[item]
 
 def order_latte():
     drink_name="latte"
     drink=MENU[drink_name]
     ingredients = drink["ingredients"]
-    if enough_resources(drink_name, ingredients["water"], ingredients["milk"],
-                        ingredients["coffee"]):
+    if enough_resources(drink_name, ingredients):
         if collect_money(drink["cost"]):
             print("Here is your drink")
-    consume_resources(ingredients["water"], ingredients["milk"],
-                        ingredients["coffee"])
+        make_drink(ingredients)
 
 def order_expresso():
     drink_name = "espresso"
     drink=MENU[drink_name]
-    ingredients = drink["ingredients"]
-    if enough_resources(drink_name, ingredients["water"], 0,
-                        ingredients["coffee"]):
+    ingridients = drink["ingredients"]
+    if enough_resources(drink_name, ingridients):
         if collect_money(drink["cost"]):
             print("Here is your drink")
-    consume_resources(ingredients["water"], 0,
-                      ingredients["coffee"])
+        make_drink(ingridients)
 
 
 def order_cappuccino():
     drink_name = "cappuccino"
     drink = MENU[drink_name]
-    ingredients = drink["ingredients"]
-    if enough_resources(drink_name, ingredients["water"], ingredients["milk"],
-                        ingredients["coffee"]):
+    ingridients = drink["ingredients"]
+    if enough_resources(drink_name, ingridients):
         if collect_money(drink["cost"]):
             print("Here is your drink")
-    consume_resources(ingredients["water"], ingredients["milk"],
-                      ingredients["coffee"])
+        make_drink(ingridients)
 
 while True:
     user_input=input("What do you want Expresso (e) / Latte (l) / Caappuccino (c)?")
